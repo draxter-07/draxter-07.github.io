f898c9168be76c8444aba02920e5513b1358c390
Philippe <philippe.idalgoprestes@gmail.com>
2023-09-19 19:24:42 -0300

    Left content animation

---
+++ leftContent/leftContentScript.js
@@ -0,0 +1,20 @@
+function showContent(){
+    let icon = event.target;
+    let resumeTextDiv = event.target.parentElement.parentElement
+    let resumeContentDiv = resumeTextDiv.parentElement
+    let resumeContentContent = resumeContentDiv.children[1]
+    let resumeContentContentHeight = resumeContentContent.style.maxHeight
+    let finalMaxHeight = "999px";
+
+    icon.style.transition = "transform 1s";
+    resumeContentContent.style.transition = "max-height 2s ease";
+
+    if (resumeContentContentHeight == finalMaxHeight){
+        icon.style.transform = "rotate(0deg)";
+        resumeContentContent.style.maxHeight = "0px";
+    }
+    else{
+        icon.style.transform = "rotate(180deg)";
+        resumeContentContent.style.maxHeight = finalMaxHeight;
+    }
+}
